d3c140a33803f0a60f3eb10d3b491bd77e785fb0
Poojabhcode <poojabhardwaj2099@gmail.com>
2022-08-16 23:11:19 +0530

    more functions of firestore

--- src/App.js
+++ src/App.js
@@ -19,15 +19,31 @@ function App() {
     //   console.log(querySnapshot);
     //  }
 
-   // 2nd---------------
-   async function f(){
-         let querySnapshot = await firestore.collection("users").get();
-         for(let i=0;i<querySnapshot.length;i++){
-          console.log(querySnapshot.docs[i].data());
-         }
+   // 2nd---------------get all----
+  //  async function f(){
+  //        let querySnapshot = await firestore.collection("users").get();
+  //        for(let i=0;i<querySnapshot.length;i++){
+  //         console.log(querySnapshot.docs[i].data());
+  //        }
         
          
-        }
+  //       }f();
+
+
+
+  //   get single
+
+    // this gives you refrence of that object
+    let f = async () =>{
+     let docRef = firestore.collection("users").doc("vWij4Aiszfrc7XrLfQ8e");
+     let documentSnapshot = await docRef.get();
+     console.log(documentSnapshot.data()); //get particular data by giving its id
+     //id nikalne k liye-
+     console.log(documentSnapshot.id);
+     //to check id fake hai ya real hai
+     console.log(documentSnapshot.exist);
+    };
+    f();
    },[]);
   
   return (
