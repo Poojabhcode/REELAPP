0204f77e57e1b6522b5aedecb95eb9115c4324c4
Poojabhcode <poojabhardwaj2099@gmail.com>
2022-08-28 13:08:38 +0530

    added user to firestore

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import AuthProvider from "./AuthProvider";
 import { useEffect } from "react";
 import { firestore } from "./firebase";
 
-function App() {
+let App= ()=> {
 
-    useEffect(() => {
+   // useEffect(() => {
 
       //add
      //firestore.collection("users").add({body: "this is value 2"});
@@ -34,22 +34,22 @@ function App() {
   //   get single
 
     // this gives you refrence of that object
-    let f = async () =>{
-     let docRef = firestore.collection("users").doc("vWij4Aiszfrc7XrLfQ8e");
-     let documentSnapshot = await docRef.get();
-     console.log(documentSnapshot.data()); //get particular data by giving its id
+    // let f = async () =>{
+    //  let docRef = firestore.collection("users").doc("vWij4Aiszfrc7XrLfQ8e");
+    //  let documentSnapshot = await docRef.get();
+    //  console.log(documentSnapshot.data()); //get particular data by giving its id
      //id nikalne k liye-
-     console.log(documentSnapshot.id);
+   //  console.log(documentSnapshot.id);
      //to check id fake hai ya real hai
-     console.log(documentSnapshot.exist);
-    };
-    f();
-   },[]);
+    //  console.log(documentSnapshot.exist);
+    // };
+    // f();
+  // },[]);
   
   return (
     <>
-    <h1>App</h1>
-    {/* <AuthProvider>
+  
+    <AuthProvider>
     <Router>
 
         <Routes>
@@ -60,7 +60,7 @@ function App() {
       
 
     </Router>
-    </AuthProvider> */}
+    </AuthProvider>
     </>
   );
 }
