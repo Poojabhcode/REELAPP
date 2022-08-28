eef959f6ee8d9fdc5626a263ece89465ded383c0
Poojabhcode <poojabhardwaj2099@gmail.com>
2022-08-28 23:50:00 +0530

    partial ui added

--- src/components/videoCard.jsx
+++ src/components/videoCard.jsx
@@ -3,24 +3,65 @@ import "./videoCard.css";
 let VideoCard = () => {
 
     let [playing, setPlaying]= useState(false);
-    return <div className="video-card">
+    let [commentBoxOpen, setCommentBoxOpen] = useState(false);
 
+    return( 
+        <div className="video-card">
         <p className= "video-card-username">Fake user</p>
         <span className="video-card-music">
 
-        <span class="material-icons">music_note</span>
+        <span className="material-icons">music_note</span>
 
             <marquee >some song</marquee>
         
         </span>
 
-        <span class="material-icons-outlined video-card-comment">chat</span>
-
-        <span class="material-icons-outlined video-card-like">favorite_border</span>
-
- 
-        <video
+        <span
         onClick={(e) => {
+            if (commentBoxOpen) {
+                setCommentBoxOpen(false);
+            }else{
+                setCommentBoxOpen(true);
+            }
+        }}
+        className="material-icons-outlined video-card-comment"
+        >
+        chat
+        </span>
+
+        <span className ="material-icons-outlined video-card-like">
+            favorite_border
+            </span>
+
+            { commentBoxOpen ? (
+                <div className="video-card-comment-box">
+                    <div className="actual-comments">
+                        <div className="post-user-comment">
+                            <img src="https://cdn.statusqueen.com/dpimages/thumbnail/dp%20image58-681.jpg" />
+                            <div>
+                                <h5>user name</h5>
+                                <p>This is actual comment</p>
+                            </div>
+                        </div>
+                        <div className="post-user-comment">
+                            <img src="https://cdn.statusqueen.com/dpimages/thumbnail/dp%20image58-681.jpg" />
+                            <div>
+                                <h5>user name</h5>
+                                <p>This is actual comment</p>
+                            </div>
+                        </div>
+                   </div>
+                   <div className="comment-form">
+                    <input type="text" />
+                    <button>Post</button>
+                   </div>
+                 </div>  
+             ) : (
+                ""   
+            )}
+
+        <video
+          onClick={(e) => {
             if (playing) {
                 e.currentTarget.pause();
                 setPlaying(false);
@@ -35,7 +76,9 @@ let VideoCard = () => {
         className="video-card-video">
 
         </video>
-    </div>;
+    </div>
+
+    );
 
 };
 
